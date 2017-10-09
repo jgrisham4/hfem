a32c72f99735f01a1f3f0490e57ae1f40c0d4f9e
jgrisham4 <jgrisham4@gmail.com>
2017-10-08 21:59:40 -0500

    Added shape function typeclass and some shape function types.

--- src/ShapeFcns.hs
+++ src/ShapeFcns.hs
@@ -2,23 +2,35 @@ module ShapeFcns
 (
   TensorProduct(..)
 , NaturalSimplex(..)
+, getBasis
+, n
 ) where
 
 import Basis
+
+-- Need to define mapping from i to i,j and i,j,k
+-- TODO Generalize beyond linear basis.
+shpFcnBasisMap :: Int -> Int -> [Int]
+shpFcnBasisMap nodeNum 1 = [nodeNum]
+shpFcnBasisMap nodeNum 2 | nodeNum == 0 = [0, 0]
+                         | nodeNum == 1 = [1, 0]
+                         | nodeNum == 2 = [1, 1]
+                         | nodeNum == 3 = [0, 1]
 
 -- Shape function types (dependent upon basis used)
 -- b represents the basis
 newtype TensorProduct  b = TensorProduct  b deriving (Show,Eq)
 newtype NaturalSimplex b = NaturalSimplex b deriving (Show,Eq)
 
-class ShapeFcn f where
-  getBasis :: (BasisType bt) => f -> bt
---  n     :: (Floating a) => f -> [a] -> Int -> a
+class ShapeFcnType f where
+  getBasis :: BasisType a => f a -> a
+  n        :: (BasisType a,Floating c) => f a -> [c] -> Int -> c
 --  dndXi :: (Floating a) => f -> [a] -> Int -> [a]
 
--- Instance for Tensor product using Lagrange basis
---instance ShapeFcn TensorProduct where
---  getBasis (TensorProduct b) = b
-
-  -- Linear Lagrange basis
- -- n (TensorProduct
+instance ShapeFcnType TensorProduct where
+  getBasis (TensorProduct a) = a
+  n (TensorProduct basis) coords i = psi basis (head coords) iInd * psi basis (coords !! 1) jInd
+    where
+      basisIndices = shpFcnBasisMap i 2
+      iInd = head basisIndices
+      jInd = last basisIndices
--- test/hfemTests.hs
+++ test/hfemTests.hs
@@ -3,7 +3,9 @@ import Test.Tasty.HUnit
 import Test.Tasty.Runners.JenkinsXML (jenkinsXMLRunner)
 
 import Basis
+import ShapeFcns
 import Node
+
 
 -- Main
 main :: IO ()
@@ -13,7 +15,7 @@ main = defaultMainWithIngredients ingredients tests
 
 -- Setting up tests
 tests :: TestTree
-tests = testGroup "Tests" [basisTests, nodeTests]
+tests = testGroup "Tests" [basisTests, shpFcnTests, nodeTests]
 
 {-
 -----------------------------------------------------------
@@ -28,7 +30,7 @@ lagrangeDPsi xi i   = Basis.dpsidxi linearLagrangeBasis xi i 1
 lagrangeD2Psi xi i  = Basis.dpsidxi linearLagrangeBasis xi i 2
 
 -- Linear Lagrange basis tests
-basisTests = testGroup "Basis Tests"
+basisTests = testGroup "Basis tests"
   [ testCase "Linear Lagrange  xi=-1, i=0" $ assertEqual "returns 1 for xi=-1, i=0" ( 1.0 :: Double) (lagrangePsi   (-1.0 :: Double) 0)
   , testCase "Linear Lagrange   xi=1, i=0" $ assertEqual "returns 0 for  xi=1, i=0" ( 0.0 :: Double) (lagrangePsi   ( 1.0 :: Double) 0)
   , testCase "Linear Lagrange  xi=-1, i=1" $ assertEqual "returns 0 for xi=-1, i=1" ( 0.0 :: Double) (lagrangePsi   (-1.0 :: Double) 1)
@@ -37,6 +39,34 @@ basisTests = testGroup "Basis Tests"
   , testCase "Linear Lagrange   ddxi, i=1" $ assertEqual "returns  1/2 for i=1"     ( 0.5 :: Double) (lagrangeDPsi  ( 0.0 :: Double) 1)
   , testCase "Linear Lagrange d2dxi2, i=0" $ assertEqual "returns 0 for i=0"        ( 0.0 :: Double) (lagrangeD2Psi ( 0.0 :: Double) 0)
   , testCase "Linear Lagrange d2dxi2, i=1" $ assertEqual "returns 0 for i=1"        ( 0.0 :: Double) (lagrangeD2Psi ( 0.0 :: Double) 1)
+  ]
+
+{-
+-----------------------------------------------------------
+Shape function tests
+-----------------------------------------------------------
+-}
+
+tPShpFcn = TensorProduct linearLagrangeBasis
+
+shpFcnTests = testGroup "Shape function tests"
+  [ testCase "Lagrange tensor prod getBasis" $ assertEqual "returns Lagrange 1" linearLagrangeBasis (getBasis tPShpFcn)
+  , testCase "Lagrange tensor prod 0, i=0"   $ assertEqual "returns 1 for xi=-1, eta=-1, i=0" 1.0 (n tPShpFcn [-1.0,-1.0] 0)
+  , testCase "Lagrange tensor prod 1, i=0"   $ assertEqual "returns 0 for xi= 1, eta=-1, i=0" 0.0 (n tPShpFcn [ 1.0,-1.0] 0)
+  , testCase "Lagrange tensor prod 2, i=0"   $ assertEqual "returns 0 for xi= 1, eta= 1, i=0" 0.0 (n tPShpFcn [ 1.0, 1.0] 0)
+  , testCase "Lagrange tensor prod 3, i=0"   $ assertEqual "returns 0 for xi=-1, eta= 1, i=0" 0.0 (n tPShpFcn [-1.0, 1.0] 0)
+  , testCase "Lagrange tensor prod 0, i=1"   $ assertEqual "returns 0 for xi=-1, eta=-1, i=1" 0.0 (n tPShpFcn [-1.0,-1.0] 1)
+  , testCase "Lagrange tensor prod 1, i=1"   $ assertEqual "returns 1 for xi= 1, eta=-1, i=1" 1.0 (n tPShpFcn [ 1.0,-1.0] 1)
+  , testCase "Lagrange tensor prod 2, i=1"   $ assertEqual "returns 0 for xi= 1, eta= 1, i=1" 0.0 (n tPShpFcn [ 1.0, 1.0] 1)
+  , testCase "Lagrange tensor prod 3, i=1"   $ assertEqual "returns 0 for xi=-1, eta= 1, i=1" 0.0 (n tPShpFcn [-1.0, 1.0] 1)
+  , testCase "Lagrange tensor prod 0, i=2"   $ assertEqual "returns 0 for xi=-1, eta=-1, i=2" 0.0 (n tPShpFcn [-1.0,-1.0] 2)
+  , testCase "Lagrange tensor prod 1, i=2"   $ assertEqual "returns 0 for xi= 1, eta=-1, i=2" 0.0 (n tPShpFcn [ 1.0,-1.0] 2)
+  , testCase "Lagrange tensor prod 2, i=2"   $ assertEqual "returns 1 for xi= 1, eta= 1, i=2" 1.0 (n tPShpFcn [ 1.0, 1.0] 2)
+  , testCase "Lagrange tensor prod 3, i=2"   $ assertEqual "returns 0 for xi=-1, eta= 1, i=2" 0.0 (n tPShpFcn [-1.0, 1.0] 2)
+  , testCase "Lagrange tensor prod 0, i=3"   $ assertEqual "returns 0 for xi=-1, eta=-1, i=3" 0.0 (n tPShpFcn [-1.0,-1.0] 3)
+  , testCase "Lagrange tensor prod 1, i=3"   $ assertEqual "returns 0 for xi= 1, eta=-1, i=3" 0.0 (n tPShpFcn [ 1.0,-1.0] 3)
+  , testCase "Lagrange tensor prod 2, i=3"   $ assertEqual "returns 0 for xi= 1, eta= 1, i=3" 0.0 (n tPShpFcn [ 1.0, 1.0] 3)
+  , testCase "Lagrange tensor prod 3, i=3"   $ assertEqual "returns 1 for xi=-1, eta= 1, i=3" 1.0 (n tPShpFcn [-1.0, 1.0] 3)
   ]
 
 {-
@@ -55,7 +85,7 @@ nodeCoords2 = [1.0 :: Double, 2.0 :: Double]
 node1d = Node nodeNum1 nodeCoords1
 node2d = Node nodeNum2 nodeCoords2
 
-nodeTests = testGroup "Node Tests"
+nodeTests = testGroup "Node tests"
   [ testCase "1d Node number"      $ assertEqual "returns 1"      nodeNum1 (nodeNumber node1d)
   , testCase "2d Node number"      $ assertEqual "returns 10"     nodeNum2 (nodeNumber node2d)
   , testCase "1d Node coordinates" $ assertEqual "returns coords" nodeCoords1 (nodeCoordinates node1d)
