6440c15464e8ee7e814b5650b76231544354fb41
jgrisham4 <jgrisham4@gmail.com>
2017-11-01 19:39:31 -0700

    Working on adding boundary elements to the mesh type.

--- src/Mesh.hs
+++ src/Mesh.hs
@@ -12,15 +12,41 @@ import Node
 import Element
 
 -- Type for a mesh
-newtype Mesh e a = Mesh [e a] deriving (Show,Eq)
+-- Mesh [Elements] [boundaryElements]
+newtype Mesh e a = Mesh [e a] [e a] deriving (Show,Eq)
 
 -- Function for getting elements from a mesh
 getMeshElements :: (Floating a,Element e) => Mesh e a -> [e a]
-getMeshElements (Mesh elements) = elements
+getMeshElements (Mesh elems _) = elems
+
+-- Function for getting the boundary elements from a mesh
 
 -- Function for getting nodes from a mesh
 getMeshNodes :: (Floating a,Eq a,Element e) => Mesh e a -> [Node a]
-getMeshNodes (Mesh elements) = nub $ concatMap getElementNodes elements
+getMeshNodes (Mesh elems _) = nub $ concatMap getElementNodes elems
+
+-- Function for getting the global node numbers for boundary elements
+belemNodeNums :: Int -> [Int] -> [[[Int]]]
+belemNodeNums 1 npts = [[[0]], [[last npts - 1]]]
+belemNodeNums 2 npts = [left, right, lower, upper]
+  where
+    imax  = head npts
+    jmax  = npts !! 1
+    left  = [[j*imax, (j+1)*imax]                 | j <- [0..(jmax-2)]]
+    right = [[j*imax+imax-1, (j+1)*imax+imax-1]   | j <- [0..(jmax-2)]]
+    lower = [[i, i+1]                             | i <- [0..(imax-2)]]
+    upper = [[(jmax-1)*imax+i, (jmax-1)*imax+i+1] | i <- [0..(imax-2)]]
+belemNodeNums 3 npts = [left, right, lower, upper, front, back]
+  where
+    imax  = head npts
+    jmax  = npts !! 1
+    kmax  = npts !! 2
+    left  = [[0]]
+    right = [[0]]
+    lower = [[0]]
+    upper = [[0]]
+    front = [[0]]
+    back  = [[0]]
 
 -- Function for getting global node numbers for each element
 elemNodeNums :: Int -> [Int] -> [[Int]]
@@ -42,13 +68,16 @@ elemNodeNums 3 npts = concat $ concat [[[[k*jmax*imax+j*imax+i,k*jmax*imax+j*ima
       kmax = npts !! 2
 
 -- Function for generating an n-dimensional mesh
+--belemNodeNums :: Int -> [Int] -> [[[Int]]]
 generateMesh :: (Fractional a, Element e) => [a] -> [a] -> [Int] -> ([Node a] -> Int -> e a) -> Mesh e a
-generateMesh xInitial xFinal npts elemConstructor = Mesh elements
+generateMesh xInitial xFinal npts elemConstructor = Mesh elems belems
   where
     dim = length xInitial
     nodeCoords = mapM (\(lo, hi, n) -> [lo + (fromIntegral v/fromIntegral (n-1)) * hi | v <- [0..(n-1)]]) (zip3 xInitial xFinal npts)
     nodes = [Node i (nodeCoords !! i) | i <- [0..(length nodeCoords - 1)]]
     numNodes = length nodes
     nelem = product $ map (\x -> x-1) npts
+    belemNodes = [[[nodes !! n | n <- nn] | nn <- bNodeNums] | bNodeNums <- belemNodeNums dim npts]
     elemNodes = [[nodes !! n | n <- nn] | nn <- elemNodeNums dim npts]
-    elements = [elemConstructor (elemNodes !! i) i| i <- [0..(nelem-1)]]
+    elems = [elemConstructor (elemNodes !! i) i | i <- [0..(nelem-1)]]
+    belems = [[elemConstructor (belemNodes !! i) someId | i <- []]]
