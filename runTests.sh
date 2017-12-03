d065f793e301f91ddc97bdfd0f923ec88dc4b5f8
James Grisham <jgrisham@gmail.com>
2017-12-02 16:08:54 -0800

    Another attempt to fix jenkins.

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-stack --stack-root /home/james/.stack  --allow-different-user test --ta "--jxml testResults.xml"
+#stack --stack-root /home/james/.stack --system-ghc --allow-different-user test --ta "--jxml testResults.xml"
+stack --system-ghc --allow-different-user test --ta "--jxml testResults.xml"
