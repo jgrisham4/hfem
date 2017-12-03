ab5b3eb07c95f1b2dcb27e808432ef80935a7820
James Grisham <jgrisham@gmail.com>
2017-12-02 16:19:17 -0800

    Jenkins..

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 stack setup
-stack --stack-root /home/james/.stack --allow-different-user test --ta "--jxml testResults.xml"
+stack --allow-different-user test --ta "--jxml testResults.xml"
 #stack --system-ghc --allow-different-user test --ta "--jxml testResults.xml"
